71a6fdfacca65fa73d696d9f5c6eaeb97248e02d
Conrado <zaolen@gmail.com>
2020-06-02 12:14:49 +0200

    refined actions/user to show server failures in views

--- src/Redux/actions/users.js
+++ src/Redux/actions/users.js
@@ -18,7 +18,6 @@ export const login = async (user) => {
     return res;
 }
 export const getAllUsers = async () => {
-    try {
         const res = await axios.get(API_URL_2 + 'users/all', {
             headers: {
                 Authorization: HEADER
@@ -29,13 +28,9 @@ export const getAllUsers = async () => {
             payload: res.data
         })
         return res;
-    } catch (error) {
-        console.error(error)
-    }
 }
 
 export const getUserDetail = async () => {
-    try {
         const res = await axios.get(API_URL_2 + 'users', {
             headers: {
                 Authorization: HEADER
@@ -46,13 +41,9 @@ export const getUserDetail = async () => {
             payload: res.data
         });
         return res;
-    } catch (error) {
-        console.error(error)
-    }
 }
 
 export const logout = async () => {
-    try {
         await axios.get(API_URL_2 + 'users/logout', {
             headers: {
                 Authorization: HEADER
@@ -62,14 +53,9 @@ export const logout = async () => {
         store.dispatch({
             type: 'LOGOUT'
         })
-    } catch (error) {
-        console.error(error)
-    }
-
 }
 
 export const uploadImage = async (fd) => {
-    try {
         const res = await axios.post(API_URL_2 + 'users/upload', fd, {
             headers: {
                 Authorization: HEADER
@@ -80,13 +66,9 @@ export const uploadImage = async (fd) => {
             payload: res.data.user
         });
         return res;
-    } catch (error) {
-        console.error(error)
-    }
 }
 
 export const updateUser = async (user) => {
-    try {
         const res = await axios.put(API_URL_2 + 'users/update', user, {
             headers: {
                 Authorization: HEADER
@@ -97,13 +79,9 @@ export const updateUser = async (user) => {
             payload: res.data[0]
         });
         return res.data[0];
-    } catch (error) {
-        console.error(error)
-    }
 }
 
 export const deleteUser = async () => {
-    try {
         await axios.delete(API_URL_2 + 'users/delete', {
             headers: {
                 Authorization: HEADER
@@ -113,45 +91,28 @@ export const deleteUser = async () => {
         store.dispatch({
             type: 'DELETE_USER'
         });
-    } catch (error) {
-        console.error(error)
-    }
-
 }
 
 export const sendRecoverEmail = async (email) => {
-    try {
         const res = await axios.post(API_URL_2 + 'users/password/forgotten', { email })
         return res;
-    } catch (error) {
-        console.error(error)
-    }
 }
 
 export const recoverPassword = async (token, password) => {
-    try {
         const res = await axios.post(API_URL_2 + 'users/password/reset', { token, password });
         return res;
-    } catch (error) {
-        console.error(error)
-    }
 }
 
 export const confirmMail = async () => {
-    try {
         const res = await axios.get(API_URL_2 + 'users/confirm-mail', {
             headers: {
                 Authorization: HEADER
             }
         })
         return res;
-    } catch (error) {
-        console.error(error)
-    }
 }
 
 export const confirmedEmail = async (token) => {
-    try {
         const res = await axios.post(API_URL_2 + 'users/mail-confirmed', { token }, {
             headers: {
                 Authorization: HEADER
@@ -162,7 +123,4 @@ export const confirmedEmail = async (token) => {
             payload: res.data[0]
         });
         return res;
-    } catch (error) {
-        console.error(error)
-    }
 }
