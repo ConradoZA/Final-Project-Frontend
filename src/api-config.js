ecb7566613c64a26831db7efc0df81ecfb72e1cc
Conrado <zaolen@gmail.com>
2020-06-19 15:29:19 +0200

    api-config is no longer hidden

--- src/api-config.js
+++ src/api-config.js
@@ -4,10 +4,10 @@ if (window.location.href.includes('localhost')) {
     backendHost1 = 'http://localhost:3001';
     backendHost2 = 'http://localhost:8000';
 } else {
-    backendHost1 = 'your-domain';
-    backendHost2 = 'your-domain';
+    backendHost1 = 'https://play-two-games-mongo.herokuapp.com';
+    backendHost2 = 'https://play-two-games-users.herokuapp.com';
 }
 
 export const API_URL_1 = `${backendHost1}/`;
-export const API_URL_2 = `${backendHost2}/`;
+export const API_URL_2 = `${backendHost2}/api/v1/`;
 export const API_URL_IMAGES = `${backendHost2}/images/`
